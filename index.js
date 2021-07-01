23c4146e545a68f60cc877d4cee577e0a345c85e
SujayAmberkar <sujayamberkar@gmail.com>
2021-07-01 15:11:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server)
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+    
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    console.log(socket);
+  });
+
+server.listen(3000,()=>{
+    console.log("listening at port 3000");
+})
